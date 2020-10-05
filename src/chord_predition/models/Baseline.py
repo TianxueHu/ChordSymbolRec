7639a3b5ed7350c4e80cba87b66dfbfa8e097ca3
YihaoChen96 <yc2288@cornell.edu>
2020-10-05 17:08:39 -0400

    add code framework

---
+++ src/chord_predition/models/Baseline.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import numpy as np
+
+class Baseline(nn.Module):
+    def __init__(self, hidden_dim, output_dim):
+        pass
+
+    def forward(self, input):
+        output = None
+        return output 
+        
