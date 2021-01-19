5c16399998dc3330d008269f42b85ca42e0fd833
TianxueHu <huxxx811@umn.edu>
2021-01-18 18:30:36 -0600

    update evaluation in progress

---
+++ src/evaluation.py
@@ -0,0 +1,11 @@
+# script for evaluation 
+
+# chord root (12 pitch classes) or 21?
+# quality 
+# full name
+'''
+def seperateEval(fullname):
+    
+    input: a chord full name from ground truth or prediction, vocab from harmalysis tool
+    output, chord root, quality, full name
+    '''
