874800e5bd0ac1bf12f69959c95a7170bb0d34db
yc2288 <yc2288@cornell.edu>
2021-04-07 15:43:10 -0400

    update vec dataset

--- chord_rec/datasets/vec_datasets.py
+++ chord_rec/datasets/vec_datasets.py
@@ -1,15 +1,34 @@
 from torch.utils.data import Dataset
 
-class FlatVecDataset(Dataset):
-    def __init__(self, note_vec, chords):
+class Vec21Dataset(Dataset):
+    def __init__(self, note_vec_seq, chord_seq, vocab):
         'Initialization'
-        self.note_vec = note_vec
-        self.chords = chords
+        self.note_vec_seq = note_vec_seq
+        self.chord_seq = chord_seq
+        self.vocab = vocab
 
     def __len__(self):
         'Get the total length of the dataset'
-        return len(self.note_vec)
+        return len(self.note_vec_seq)
+    
     def __getitem__(self, index):
         'Generates one sample of data'
         # Select sample
-        return self.note_vec[index], self.chords[index]
+        
+        
+        return self.note_vec_seq[index], self.vec_encode(self.chord_seq[index])
+    
+    def encode(self, x):
+        return self.vocab.stoi[x]
+
+
+    def vec_encode(self, x):
+        return np.vectorize(self.encode)(x)
+    
+
+    def decode(self, x):
+        return self.vocab.itos[x]
+
+
+    def vec_decode(self, x):
+        return np.vectorize(self.decode)(x)
