8b5f7fd1bf5913a2445c9ed81e495c9e438dbb84
yc2288 <yc2288@cornell.edu>
2021-04-07 15:46:44 -0400

    update init

--- chord_rec/models/__init__.py
+++ chord_rec/models/__init__.py
@@ -1,3 +1,6 @@
 from .lit_baseline import *
 from .lit_seq2seq import *
 from .lit_transformer import *
+from .seq2seq.Decoder import *
+from .seq2seq.Encoder import *
+from .seq2seq.Seq2Seq import *
