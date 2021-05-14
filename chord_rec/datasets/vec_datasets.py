cc83046c593d56fea56c439fc81d260363de4855
yc2288 <yc2288@cornell.edu>
2021-05-14 16:32:40 -0400

    add ffnn dataset

--- chord_rec/datasets/vec_datasets.py
+++ chord_rec/datasets/vec_datasets.py
@@ -33,3 +33,18 @@ class Vec45Dataset(Dataset):
 
     def vec_decode(self, x):
         return np.vectorize(self.decode)(x)
+
+
+class FFNNDataset(Dataset):
+    def __init__(self, note_vec, chords):
+        'Initialization'
+        self.note_vec = note_vec
+        self.chords = chords
+
+    def __len__(self):
+        'Get the total length of the dataset'
+        return len(self.note_vec)
+    def __getitem__(self, index):
+        'Generates one sample of data'
+        # Select sample
+        return self.note_vec[index].astype(np.float32), self.chords[index]
